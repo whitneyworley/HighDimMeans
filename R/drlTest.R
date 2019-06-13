4d0aa3aad04d2f6a8b18249ab7b57b53038f67f3
Whitney Worley <wworley1@uca.edu>
2019-06-13 15:28:58 -0500

    add drl test

---
+++ R/drlTest.R
@@ -0,0 +1,21 @@
+#' Diagonal Likelood Ratio Test
+#'
+#' @param x Data set 1.
+#' @param y Data set 2.
+#'
+#' @return Diagonal Likelood Ratio test statistic for different mean vectors.
+#' @export
+
+drlTest <- function(x, y) {
+  n1 <- nrow(x)
+  n2 <- nrow(y)
+  xbar <- colMeans(x)
+  ybar <- colMeans(y)
+  nu2 <- n1 + n2 - 2
+  s <- 1 / (n1 + n2 - 2) * (
+    (x - xbar) %*% t(x - xbar) + (y - ybar) %*% t(y - ybar)
+  )
+  tsq <- sqrt(n1 * n2 / (n1 + n2)) * (xbar - ybar) / diag(s)
+  t2 <- (n1 + n2) * sum(log(1 + tsq / nu2))
+  t2
+}
